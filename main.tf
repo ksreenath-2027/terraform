202d780f3e57c500d54832aa126fad5c45c8be2a
ksreenath-2027 <ksreenath2027@gmail.com>
2026-05-04 20:05:00 +0530

    Initialized with terraform config

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-practice"
+  }
+}
+
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "terraform-state-bucket-sreenath214"
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
